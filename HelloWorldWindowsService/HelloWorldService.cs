4eddc4e102fb04fb863aaa2b7c340f72f4983ae2
marishvinoj <43641320+marishvinoj@users.noreply.github.com>
2020-03-12 19:40:56 +0530

    First Commit

---
+++ HelloWorldWindowsService/HelloWorldService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace HelloWorldWindowsService
+{
+    public class HelloWorldService
+    {
+        int milliseconds = 2000;
+        public void Start()
+        {
+            Console.WriteLine("Hello World Program Started");
+            for (int i = 0;i<3; i++)
+            {
+                Thread.Sleep(milliseconds);
+                Console.WriteLine(DateTime.Now);
+                Console.WriteLine("--------------------------");
+            }
+        }
+        public void Stop()
+        {
+            Console.WriteLine("--------------------------");
+            Console.WriteLine($"Printing current time {DateTime.Now} and this service will stoped after 2 seconds");
+            Thread.Sleep(milliseconds);
+            Console.WriteLine("Hello World Program end");
+            Console.WriteLine("--------------------------");
+        }
+    }
+}
---
+++ HelloWorldWindowsService/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Topshelf;
+
+namespace HelloWorldWindowsService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Configure();
+        }
+
+        internal static void Configure()
+        {
+            HostFactory.Run(configure =>
+            {
+                configure.Service<HelloWorldService>(service =>
+                {
+                    service.ConstructUsing(s => new HelloWorldService());
+                    service.WhenStarted(s => s.Start());
+                    service.WhenStopped(s => s.Stop());
+                });
+
+                //Setup Account that window service use to run.  
+                configure.RunAsLocalSystem();
+                //This is the Service Name which will be created in your services.msc
+                configure.SetServiceName("HelloWorldWindowsServiceWithTopshelf");
+                //This is the Display Name which will be created in your services.msc
+                configure.SetDisplayName("Windows Service With Topshelf");
+                configure.SetDescription("Hello World Windows Service with Topshelf");
+            });
+        }
+    }
+}
